a0cc160e7828169eccd5e5f472fd36541525840e
tomuyamukun <3156tomochan@gmail.com>
2021-02-20 15:49:09 +0900

    データの受け渡し

--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,7 @@
-const LANGUAGES = ["JavaScript", "C++", "Ruby", "Java", "PHP", "Go"];
-
-export const List = () => {
+export const List = ({ langs }) => {
 	return (
 		<div>
-			{LANGUAGES.map((lang, index) => {
+			{langs.map((lang, index) => {
 				return <div key={index}>{lang}</div>;
 			})}
 		</div>
