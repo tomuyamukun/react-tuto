367eb43cb64901772ad07801668f23fc20499e1a
tomuyamukun <3156tomochan@gmail.com>
2021-02-20 19:17:07 +0900

    style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import React from "react";
 import { List } from "./List";
 import { Form } from "./Form";
 import { getLanguages } from "./const/languages";
+import styled from "styled-components";
+
+const Header = styled.header`
+	display: flex;
+	justify-content: space-between;
+	padding: 24px 64px 0;
+	border-bottom: 1px solid #e0e0e0;
+`;
+
+const HeaderUl = styled.ul`
+	display: flex;
+	margin: 0;
+	padding: 0;
+`;
+
+const HeaderLi = styled.li`
+list-style:none;
+padding: 4px 12px;
+cursor: pointer;
+border-bottom${(props) => (props.focused ? "2px solid #F44336" : "none")}
+`;
 
 class App extends React.Component {
 	constructor(props) {
@@ -28,13 +49,22 @@ class App extends React.Component {
 		const { tab, langs } = this.state;
 		return (
 			<div>
-				<header>
-					<ul>
-						<li onClick={() => this.setState({ tab: "list" })}>List</li>
-						<li onClick={() => this.setState({ tab: "form" })}>Form</li>
-					</ul>
-				</header>
-				<hr />
+				<Header>
+					<HeaderUl>
+						<HeaderLi
+							focused={tab === "list"}
+							onClick={() => this.setState({ tab: "list" })}
+						>
+							List
+						</HeaderLi>
+						<HeaderLi
+							focused={tab === "form"}
+							onClick={() => this.setState({ tab: "form" })}
+						>
+							Form
+						</HeaderLi>
+					</HeaderUl>
+				</Header>
 				{tab === "list" ? (
 					<List langs={langs} />
 				) : (
--- src/List.jsx
+++ src/List.jsx
@@ -1,4 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	padding: 12px 64px;
+`;
+
+const ListItem = styled.div`
+	padding: 8px 16px;
+
+	&:nth-child(n + 2) {
+		border-top: 1px solid #d9dbde;
+	}
+`;
 
 export class List extends React.Component {
 	componentWillUnmount() {
@@ -8,11 +21,11 @@ export class List extends React.Component {
 	render() {
 		const { langs } = this.props;
 		return (
-			<div>
+			<Container>
 				{langs.map((lang, index) => {
-					return <div key={index}>{lang}</div>;
+					return <ListItem key={index}>{lang}</ListItem>;
 				})}
-			</div>
+			</Container>
 		);
 	}
 }
