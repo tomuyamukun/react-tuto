3a469fa1fca9be0e75ffad48936afa52af8e0cee
tomuyamukun <3156tomochan@gmail.com>
2021-02-21 20:52:00 +0900

    ref

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Button } from "./components/button";
 import { TabBodyContainer } from "./components/tab-body-container";
 import { FormModal } from "./FormModal";
+import { Hint } from "./Hint";
 
 const Label = styled.label`
 	display: flex;
@@ -42,6 +43,7 @@ export const Form = ({ onAddLang }) => {
 						onChange={(e) => setText(e.target.value)}
 						autoFocus
 					/>
+					<Hint />
 				</div>
 				<ButtonContainer>
 					<FormButton>追加</FormButton>
